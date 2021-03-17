48e525faf9563d82ace40d7ef21a72bd12b32794
Harold <andresdias24@gmail.com>
2021-03-17 18:50:07 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import  NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar">
+        <NavBar />
       </div>
       <div className="main-content">
         <div className="content">
+          <HomePage />
         </div> 
       </div>
     </div>
