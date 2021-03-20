77a9a76e8040c292c1324c01d6d99b016883bfbc
Harold <andresdias24@gmail.com>
2021-03-20 12:48:34 -0500

    create nav-toggle

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import  AboutPage from './Pages/AboutPage'
 import  PortfolioPage from './Pages/PortfolioPage'
 import  Contact from './Pages/Contact'
 import {Switch, Route} from 'react-router-dom'
+import {useState} from 'react'
 function App() {
+  const [NavToggle, setNavToogle] =  useState(false);
+  
+   const navClick  = () => {
+    setNavToogle(!NavToggle);
+  }
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${NavToggle ? 'nav-toggle' :  ''} `}>
         <NavBar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="line-1"></div>
+        <div className="line-2"></div>
+        <div className="line-3"></div>
       </div>
       <div className="main-content">
         <div className="content">
