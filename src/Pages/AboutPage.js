51515f827816575a34415bdc4ee85b3fc8ac51fe
Harold <andresdias24@gmail.com>
2021-04-02 18:42:28 -0500

    styles map section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -11,15 +11,13 @@ function AboutPage() {
             <ImageSection />
             <Title  tittle={'Mis habilidades'} span={'Mis habilidades'}/>   
                 <div className="skillContainer">
-                    <SkillsSection  skill={'GIT'} progress={'80%'} width={'40%'}/>
-                    <SkillsSection  skill={'javascript'} progress={'70%'} width={'70%'}/>
-                    <SkillsSection  skill={'Vue'} progress={'70%'} width={'70%'}/>
-                    <SkillsSection  skill={'MongoDB & MSQL'} progress={'70%'} width={'70%'}/>
-                    <SkillsSection  skill={'Cypress'} progress={'70%'} width={'70%'}/>
-                    <SkillsSection  skill={'Node'} progress={'50%'} width={'50%'}/>
-                    <SkillsSection  skill={'Express'} progress={'50%'} width={'50%'}/>
-                    <SkillsSection  skill={'Angular8'} progress={'50%'} width={'40%'}/>
-                    <SkillsSection  skill={'PHP'} progress={'50%'} width={'40%'}/>
+                    <SkillsSection  skill={'GIT'} progress={'70%'} width={'70%'}/>
+                    <SkillsSection  skill={'javascript'} progress={'60%'} width={'60%'}/>
+                    <SkillsSection  skill={'Vue'} progress={'60%'} width={'60%'}/>
+                    <SkillsSection  skill={'MongoDB & MSQL'} progress={'60%'} width={'60%'}/>
+                    <SkillsSection  skill={'Cypress'} progress={'60%'} width={'60%'}/>
+                    <SkillsSection  skill={'Node'} progress={'60%'} width={'60%'}/>
+                    <SkillsSection  skill={'Express'} progress={'60%'} width={'60%'}/>
                     <SkillsSection  skill={'React'} progress={'40%'} width={'40%'}/>
                     <SkillsSection  skill={'NUXT'} progress={'40%'} width={'40%'}/>
                 </div>    
