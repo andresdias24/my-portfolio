daf4ecfd390a9c05a2e470c54694d54dd98256a7
Harold Diaz <65746887+andresdias24@users.noreply.github.com>
2024-05-02 18:16:05 -0500

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -9,10 +9,10 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hola, Soy
-                    <span> Web Develop</span>
+                    <span> Andres Diaz</span>
                 </h1>
                 <p className="h-sub-text">
-                    Mi nombre es Harold Andres Vargas Diaz soy de Bogota, me desempeño como programador web actualmente cuento con una experiencia de mas de dos años en el desarrollo de app web con tecnologias como <strong>Angular8, Vue.js, React.js, node.js, PHP,</strong> tengo experiencia con la metodologia <strong>SCRUM</strong> estoy cursando la carrera de ingenieria de software,  me gusta la musica,  soy apasionado por el aprendizaje y la tecnología!!
+                    Soy web develop de la ciuidad de Bogota, actualmente cuento con una experiencia de mas de 4 años en el desarrollo de app web con tecnologias como <strong>Angular, Vue, React, node.js, MongoDB, SQL</strong> cuento conexperiencia en  metodologias <strong>AGILES</strong> me gusta la tecnologia, la innovacion, me gusta la musica,  soy apasionado por el aprendizaje!!
                 </p>
                 <div className="icons">
                     <Link to="/portfolio" className="icon-holder">
